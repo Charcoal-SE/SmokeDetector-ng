4969d8629e0d2a0a35fc384696946a96a4f70142
ArtOfCode <owen.jenkins2903@gmail.com>
2017-04-22 18:18:42 +0100

    Fix inspections

--- src/command_dispatch.py
+++ src/command_dispatch.py
@@ -8,58 +8,59 @@ import chat
 _commands = {"reply": {}, "prefix": {}}
 
 
-# noinspection PyShadowingNames,PyShadowingBuiltins,PyMissingTypeHints
-def command(*type_signature, reply=False, whole_msg=False, aliases=[]):
+# noinspection PyMissingTypeHints
+def command(*type_signature, reply=False, whole_msg=False, aliases=None):
+    if aliases is None:
+        aliases = []
+
     # noinspection PyMissingTypeHints
     def decorator(func):
-        # noinspection PyShadowingBuiltins
         def f(*args, original_msg=None) -> object:
-            processed_args = [type(arg) for type, arg in zip(type_signature, args)]
+            processed_args = [get_type(arg) for get_type, arg in zip(type_signature, args)]
 
             if whole_msg:
                 return func(original_msg, *processed_args)
             else:
                 return func(*processed_args)
 
-        command = (f, len(type_signature))
+        cmd = (f, len(type_signature))
 
         if reply:
-            _commands["reply"][func.__name__] = command
+            _commands["reply"][func.__name__] = cmd
 
             for alias in aliases:
                 _commands["reply"][alias] = command
         else:
-            _commands["prefix"][func.__name__] = command
+            _commands["prefix"][func.__name__] = cmd
 
             for alias in aliases:
-                _commands["prefix"][alias] = command
+                _commands["prefix"][alias] = cmd
 
         return f
 
     return decorator
 
 
-# noinspection PyShadowingNames,PyShadowingBuiltins
 def dispatch_command(msg) -> str:
     command_parts = msg.content.split(" ", 1)
 
     if len(command_parts) == 2:
-        command, args = command_parts
+        cmd, args = command_parts
     else:
-        command, = command_parts
+        cmd, = command_parts
         args = ""
 
-    command_name = command[len(config.command_prefix):].lower()
+    command_name = cmd[len(config.command_prefix):].lower()
 
     if command_name not in _commands["prefix"]:
         return "@%s No such command %s." % (msg.owner.name, command_name)
     else:
-        function, arity = _commands["prefix"][command_name]
+        func, arity = _commands["prefix"][command_name]
 
         if arity == 0:
-            return function(original_msg=msg)
+            return func(original_msg=msg)
         elif arity == 1:
-            return function(args, original_msg=msg)
+            return func(args, original_msg=msg)
         else:
             args = args.split()
 
@@ -68,35 +69,33 @@ def dispatch_command(msg) -> str:
             elif len(args) > arity:
                 return "Too many arguments."
             else:
-                return "@%s %s" % (msg.owner.name, function(*args, original_msg=msg))
+                return "@%s %s" % (msg.owner.name, func(*args, original_msg=msg))
 
 
-# noinspection PyShadowingNames,PyShadowingBuiltins
-def dispatch_reply_command(msg, reply, command) -> str:
-    command = command.lower()
+def dispatch_reply_command(msg, reply, cmd) -> str:
+    cmd = cmd.lower()
 
-    if command not in _commands["reply"]:
-        return "@%s No such command %s." % (reply.owner.name, command)
+    if cmd not in _commands["reply"]:
+        return "@%s No such command %s." % (reply.owner.name, cmd)
     else:
-        function, arity = _commands["reply"][command]
+        func, arity = _commands["reply"][cmd]
 
         assert arity == 1
 
-        return "@%s %s" % (reply.owner.name, function(msg.id, original_msg=reply))
+        return "@%s %s" % (reply.owner.name, func(msg.id, original_msg=reply))
 
 
-# noinspection PyShadowingNames,PyShadowingBuiltins
 def dispatch_shorthand_command(msg, room) -> str:
     commands = msg.content[len(config.shorthand_prefix):].split()
 
     output = []
     processed_commands = []
 
-    for command in commands:
-        count, command = regex.match(r"^(\d*)(.*)", command).groups()
+    for cmd in commands:
+        count, cmd = regex.match(r"^(\d*)(.*)", cmd).groups()
 
         for _ in range(int(count) if count else 1):
-            processed_commands.append(command)
+            processed_commands.append(cmd)
 
     for current_command, message in zip(processed_commands, chat.get_last_messages(room, len(processed_commands))):
         if current_command != "-":
