9766f442f0507a1e9cb7549f3815a4b2fd226ef0
quartata <quartata0@gmail.com>
2017-04-23 10:28:01 -0700

    Start WS in separate thread (and separate stack most importantly), remove "Re-opened websocket" messages

--- src/ws.py
+++ src/ws.py
@@ -1,5 +1,6 @@
 # vim: set filetype=python tabstop=4 shiftwidth=4 expandtab:
 
+import threading
 import websocket
 
 import chat
@@ -8,7 +9,7 @@ import chat
 def start_event_loop():
     socket = websocket.WebSocketApp("ws://qa.sockets.stackexchange.com", on_open=init_ws, on_message=lambda *x: None,
                                     on_close=restart_ws)
-    socket.run_forever()
+    threading.Thread(target=socket.run_forever).start()
 
 
 def init_ws(socket):
@@ -16,5 +17,4 @@ def init_ws(socket):
 
 
 def restart_ws(_):
-    chat.tell_rooms_with("debug", "Re-opened SE websocket.")
     start_event_loop()
