005732098f67a41cef71e1616f34dbb8d09d0a3b
ArtOfCode- <hello@artofcode.co.uk>
2017-05-08 18:32:59 +0100

    More bodyfetcher

--- src/bodyfetcher.py
+++ src/bodyfetcher.py
@@ -1,25 +1,70 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from time import sleep
+from threading import Lock
 from sqlalchemy import func
 import requests
+from chat import tell_rooms_with
+from post import Post
 import config
+import secrets
 from database import SESSION, BaseModel, BodyfetcherQueueItem, BodyfetcherQueueTiming, BodyfetcherMaxId
 from spam_handling import check_if_spam
 
 
+API_REQUEST_LOCK = Lock()
+BACKOFF_UNTIL = datetime(1970, 1, 1, 0, 0, 0)
+
+
 def _queue_threshold(site):
-    overridden = config['queue_sizes']
+    overridden = config.queue_sizes
     if site in overridden:
         return overridden[site]
     else:
-        return config['default_queue_size']
+        return config.default_queue_size
 
 
 def _fire_api_request(items, site):
     item_ids = [x.id for x in items.all()]
     previous_max_id = SESSION.query(func.max(BodyfetcherMaxId)).filter(BodyfetcherMaxId.site_url == site).all()[0][0]
-    intermediate_ids = range(previous_max_id + 1, min(item_ids))
+    intermediate_ids = list(range(previous_max_id + 1, min(item_ids)))
     fetch_ids = intermediate_ids + item_ids
     fetch_ids = fetch_ids[-min(len(fetch_ids), 100):]
+
+    api_key = secrets['se_key']
+    uri = 'https://api.stackexchange.com/2.2/questions/{}?site={}&key={}'.format(';'.join(fetch_ids), site, api_key)
+
+    API_REQUEST_LOCK.acquire()
+    if datetime.utcnow() < BACKOFF_UNTIL:
+        sleep((BACKOFF_UNTIL - datetime.utcnow()).total_seconds())
+
+    try:
+        response = requests.get(uri, timeout=10)
+    except (requests.exceptions.Timeout, requests.ConnectionError) as ex:
+        tell_rooms_with('debug', "SE API request errored: {}".format(ex))
+        return
+
+    API_REQUEST_LOCK.release()
+    response = response.json()
+
+    _clear_queue(fetch_ids, site)
+    _process_response(response)
+
+
+def _clear_queue(ids, site):
+    col = SESSION.query(BodyfetcherQueueItem).filter(BodyfetcherQueueItem.post_id.in_(ids),
+                                                     BodyfetcherQueueItem.site_url == site)
+    BaseModel.delete_collection(col)
+
+
+def _process_response(response):
+    global BACKOFF_UNTIL
+
+    if 'backoff' in response:
+        BACKOFF_UNTIL = datetime.utcnow() + timedelta(seconds=response['backoff'])
+
+    for post in response['items']:
+        post_obj = Post(api_response=post)
+        check_if_spam(post_obj)
 
 
 def _calculate_queue_timings(items):
